4cd4e5ba1427c2fd6c0a8e73f0d5964796662133
redoger121 <english121ru123>
2024-08-08 19:24:12 +0700

    Updates

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './GlobalContextProvider';
-export * from './TargetText';
-export * from './InputText';
-export * from './Results';
-export * from './RestartButton'
-export * from './UI'
